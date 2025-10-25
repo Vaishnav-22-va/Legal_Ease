30000aadce7ce81fac0d0c9dc85839d6ee3e59d1
Vaishnav-22-va <vaishnavsen59@gmail.com>
2025-10-25 17:17:29 +0530

    Update asgi.py

--- legalmunshi_backend/asgi.py
+++ legalmunshi_backend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legalmunshi_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legal_ease.legalmunshi_backend.settings')
 
 application = get_asgi_application()
