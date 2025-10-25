4448c708dc87d427e12079bda3065fd19b02df71
Vaishnav-22-va <vaishnavsen59@gmail.com>
2025-10-25 20:28:44 +0530

    Updated templates for static and collected static files

--- legalmunshi_backend/asgi.py
+++ legalmunshi_backend/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legal_ease.legalmunshi_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legalmunshi_backend.settings')
+
 
 application = get_asgi_application()
